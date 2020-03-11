93c9e5715b9d25b86dd28e0a1a82c5d8958f69e5
komite987 <komait.salamah@gmail.com>
2020-03-11 10:27:03 +0200

    Add Hirb gem, user and message model and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Samer", password: "123456", password_confirmation: "123456")
+User.create(username: "Sami", password: "123456", password_confirmation: "123456")
+User.create(username: "Rami", password: "123456", password_confirmation: "123456")
+User.create(username: "Fadi", password: "123456", password_confirmation: "123456")
+
+
+Message.create(body: "Hi everyone", user_id: 10)
+Message.create(body: "Fine thx", user_id: 11)
+Message.create(body: "Fine and you?", user_id: 12)
+Message.create(body: "welcome", user_id: 13)
+Message.create(body: "I miss you", user_id: 10)
